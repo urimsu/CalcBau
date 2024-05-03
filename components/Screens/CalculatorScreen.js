6f63f7f62d551b34ffd113d0ce6aa7f237fbdb72
urimsu <urimsu56@gmail.com>
2024-05-03 23:50:35 +0200

    Calculator Screen update

--- components/Screens/CalculatorScreen.js
+++ components/Screens/CalculatorScreen.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { 
   Text, 
   View, 
-  StyleSheet
+  StyleSheet,
+  TextInput
 } 
   from 'react-native';
 
+  function addZehn(n){
+    return parseInt(n)+10;
+  }
+  
+  function mal(n,m){
+    return parseInt(n)*parseInt(m);
+  }
 
   function CalculatorScreen() {
+    const [preis,setPreis]=useState();
+    const [qm,setQm]=useState();
     return (
       <View style={styles.Container}>
         <Text style={styles.Text}>Calc Screen</Text>
+        <TextInput style={{borderWidth:1}} value={preis} onChangeText={setPreis} keyboardType='numeric'></TextInput>
+        <TextInput style={{borderWidth:1}} value={qm} onChangeText={setQm} keyboardType='numeric'></TextInput>
+        <Text>{ preis != 0 && qm != 0 ? mal(preis,qm) : 0}€</Text>
       </View>
     );
   }
